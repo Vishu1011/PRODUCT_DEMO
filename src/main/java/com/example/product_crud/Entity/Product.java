c2feb4118e592b304bb26c271d5dff2d72ff4b68
Vishu@1011 <vidhiok@gmail.com>
2025-02-27 17:25:21 -0800

    swagger dependency add

--- src/main/java/com/example/product_crud/Entity/Product.java
+++ src/main/java/com/example/product_crud/Entity/Product.java
@@ -1,16 +1,16 @@
 package com.example.product_crud.Entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
 
 import jakarta.persistence.*;
-import lombok.NoArgsConstructor;
+//import lombok.NoArgsConstructor;
 
 @Entity
-@Data
-@Table(name="PRODUCT")
-@AllArgsConstructor
-@NoArgsConstructor
+//@Data
+//@Table(name="PRODUCT")
+//@AllArgsConstructor
+//@NoArgsConstructor
 
 public class Product {
 
@@ -22,4 +22,40 @@ public class Product {
     private String productType;
     private String productDescription;
     private Double price;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getProductType() {
+        return productType;
+    }
+
+    public void setProductType(String productType) {
+        this.productType = productType;
+    }
+
+    public String getProductDescription() {
+        return productDescription;
+    }
+
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
 }
